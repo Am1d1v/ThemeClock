6f6ecd95ab5213c05c4238bb638f3bf35f634d21
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-12-08 19:54:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
-const hour = document.querySelector('.hour');
-const minute = document.querySelector('.minute');
-const second = document.querySelector('.second');
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
@@ -22,7 +22,27 @@ toggle.addEventListener('click', (e) => {
     }
 })
 
+// Clock
+function setTime(){
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const hoursForClock = hourEl % 12;
 
+    // Clock Rotation
+    const scale = (num, in_min, in_max, out_min, out_max) => {
+        return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }   
 
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hours, 0, 11, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
 
+}
+setTime();
+
+setInterval(setTime, 1000);
 
